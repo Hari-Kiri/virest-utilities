9e5f839f166d2b423cead527c0e9c3b1c77d0646
Hari-Kiri <pratamahari89@gmail.com>
2024-12-29 15:39:36 +0000

    fix: rollback filename and interface name

--- utils/ResponseStructure.go
+++ utils/ResponseStructure.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"github.com/Hari-Kiri/virest-storage-pool/structures/authenticate"
 	"github.com/Hari-Kiri/virest-storage-pool/structures/getGid"
 	"github.com/Hari-Kiri/virest-storage-pool/structures/getUid"
 	"github.com/Hari-Kiri/virest-storage-pool/structures/poolAutostart"
@@ -17,20 +18,21 @@ import (
 	"github.com/Hari-Kiri/virest-storage-pool/structures/poolUndefine"
 )
 
-// Defined generic type constraint for request model structure.
-type requestStructure interface {
-	getUid.Request |
-		getGid.Request |
-		poolList.Request |
-		poolInfo.Request |
-		poolDetail.Request |
-		poolDefine.Request |
-		poolBuild.Request |
-		poolCreate.Request |
-		poolAutostart.Request |
-		poolDestroy.Request |
-		poolDelete.Request |
-		poolUndefine.Request |
-		poolRefresh.Request |
-		poolCapabilities.Request
+// Defined generic type constraint for response model structure.
+type ResponseStructure interface {
+	authenticate.Response |
+		getUid.Response |
+		getGid.Response |
+		poolList.Response |
+		poolInfo.Response |
+		poolDetail.Response |
+		poolDefine.Response |
+		poolBuild.Response |
+		poolCreate.Response |
+		poolAutostart.Response |
+		poolDestroy.Response |
+		poolDelete.Response |
+		poolUndefine.Response |
+		poolRefresh.Response |
+		poolCapabilities.Response
 }
